b46695a4ba27621895c0b89b043e793d48dd473f
edawa123456-cell <edawa123456@gmail.com>
2026-09-25 10:41:57 -0500

    Update route.js

--- app/api/send-tow-request/route.js
+++ app/api/send-tow-request/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
   try {
     const body = await request.json();
-
+console.log("TOW REQUEST BODY:", body);
     const {
       customerName,
       customerPhone,
